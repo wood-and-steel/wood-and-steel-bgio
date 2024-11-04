8cb4c5e1c6839cc4107ea03a50faa03e9ce244e8
Jeff Pettiross <jeff@observablehq.com>
2024-11-03 23:22:37 -0800

    Wire in a little bit of hotseat, preparing for contracts to belong to players

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Client } from 'boardgame.io/react';
+import { Local } from 'boardgame.io/multiplayer';
 import { WoodAndSteel } from './Game';
 import { WoodAndSteelState } from './Board';
 
-const App = Client({ 
+const WoodAndSteelClient = Client({ 
   game: WoodAndSteel,
   board: WoodAndSteelState,
+  multiplayer: Local(),
 });
+
+const App = () => (
+  <div>
+    <WoodAndSteelClient playerID="0" />
+  </div>
+);
 
 export default App;
--- src/Game.js
+++ src/Game.js
@@ -1,22 +1,31 @@
 import Contract from "./Contract"
+import { TurnOrder } from 'boardgame.io/core';
 
 export const WoodAndSteel = {
+  name: "wood-and-steel",
+  
   setup: () => ({ 
     contracts: Array(0),
   }),
 
   moves: {
 
-    generateStartingContract: ({ G }, activeCities) => {
+    generateStartingContract: ({ G, playerID }, activeCities) => {
       // TODO: Remove generateStaartingContract. Wired it up this way for now to work around my lack of React skill.
       const contract = Contract.generateStartingContract(G, activeCities);
       G.contracts.push(contract.toJSON());
     },
 
-    generateMarketContract: ({ G }, activeCities) => {
+    generateMarketContract: ({ G, playerID }, activeCities) => {
       // TODO: Remove generateMarketContract. Wired it up this way for now to work around my lack of React skill.
       const contract = Contract.generateMarketContract(G, activeCities);
       G.contracts.push(contract.toJSON());
     },
   },
+
+  turn: {
+    // HACK: Storing multiple players in G for contracts, but only rendeirng client
+    // for player 0, so we want to make sure it's always player 0's turn for now.
+    order: TurnOrder.CUSTOM(['0']),
+  }
 };
