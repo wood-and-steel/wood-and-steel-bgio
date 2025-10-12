7db213d35e1b0d1e8da91c28d577efec6c9a3a21
Jeff Pettiross <jeff@pettiross.com>
2025-10-11 21:57:32 -0700

    Fix typo in fliename

--- src/Board.js
+++ src/Board.js
@@ -2,6 +2,62 @@ import React from "react";
 import { cities, commodities } from "./GameData";
 import { valueOfCity, rewardValue, railroadTieValue } from "./Contract";
 
+// Import commodity icons
+import aluminumIcon from './images/aluminum.svg';
+import bauxiteIcon from './images/bauxite.svg';
+import cattleIcon from './images/cattle.svg';
+import coalIcon from './images/coal.svg';
+import copperIcon from './images/copper.svg';
+import cottonIcon from './images/cotton.svg';
+import fishIcon from './images/fish.svg';
+import fruitIcon from './images/fruit.svg';
+import grainIcon from './images/grain.svg';
+import importsIcon from './images/imports.svg';
+import ironOreIcon from './images/iron_ore.svg';
+import leadIcon from './images/lead.svg';
+import machineryIcon from './images/machinery.svg';
+import nickelIcon from './images/nickel.svg';
+import oilIcon from './images/oil.svg';
+import porkIcon from './images/pork.svg';
+import preciousMetalsIcon from './images/precious_metals.svg';
+import riceIcon from './images/rice.svg';
+import sheepIcon from './images/sheep.svg';
+import steelIcon from './images/steel.svg';
+import textilesIcon from './images/textiles.svg';
+import tobaccoIcon from './images/tobacco.svg';
+import touristsIcon from './images/tourists.svg';
+import wineIcon from './images/wine.svg';
+import woodIcon from './images/wood.svg';
+
+// Map commodity names to their icons
+const commodityIcons = {
+  'aluminum': aluminumIcon,
+  'bauxite': bauxiteIcon,
+  'cattle': cattleIcon,
+  'coal': coalIcon,
+  'copper': copperIcon,
+  'cotton': cottonIcon,
+  'fish': fishIcon,
+  'fruit': fruitIcon,
+  'grain': grainIcon,
+  'imports': importsIcon,
+  'iron ore': ironOreIcon,
+  'lead': leadIcon,
+  'machinery': machineryIcon,
+  'nickel': nickelIcon,
+  'oil': oilIcon,
+  'pork': porkIcon,
+  'precious metals': preciousMetalsIcon,
+  'rice': riceIcon,
+  'sheep': sheepIcon,
+  'steel': steelIcon,
+  'textiles': textilesIcon,
+  'tobacco': tobaccoIcon,
+  'tourists': touristsIcon,
+  'wine': wineIcon,
+  'wood': woodIcon,
+};
+
 // Style constants
 const contractStyles = {
   enabled: {
@@ -190,7 +246,11 @@ function ReferenceTables({ G }) {
   );
 
   const commodityList = [...commodities].map(([key, value]) =>
-    <div key={key}>{key} <span style={{opacity: "0.6"}}>• {formatCommodityList(value.cities)}</span></div>
+    <div key={key} style={{display: 'flex', alignItems: 'center', gap: '0.5rem'}}>
+      <img src={commodityIcons[key]} alt={key} style={{width: '24px', height: '24px'}} />
+      <span>{key}</span>
+      <span style={{opacity: "0.6"}}>• {formatCommodityList(value.cities)}</span>
+    </div>
   );
 
   return (
